f9b6fc4bfae4b710d8a8082c5e63c7bfa04ab2c2
Edoardo Zoni <ezoni@lbl.gov>
2025-03-31 13:49:03 -0700

    Remove temporary test files

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import ValidationError
 
 import json
@@ -64,14 +65,18 @@ def test_yaml():
     # Serialize the Line object to YAML
     yaml_data = yaml.dump(line.model_dump(), default_flow_style=False)
     print(f"\n{yaml_data}")
-    # Write the YAML data to a file
-    with open("line.yaml", "w") as file:
+    # Write the YAML data to a test file
+    test_file = "line.yaml"
+    with open(test_file, "w") as file:
         file.write(yaml_data)
-    # Read the YAML data from the file
-    with open("line.yaml", "r") as file:
+    # Read the YAML data from the test file
+    with open(test_file, "r") as file:
         yaml_data = yaml.safe_load(file)
     # Parse the YAML data back into a Line object
     loaded_line = Line(**yaml_data)
+    # Remove the test file
+    os.remove(test_file)
+    # Validate loaded Line object
     assert line == loaded_line
 
 
@@ -85,12 +90,16 @@ def test_json():
     # Serialize the Line object to JSON
     json_data = json.dumps(line.model_dump(), sort_keys=True, indent=2)
     print(f"\n{json_data}")
-    # Write the JSON data to a file
-    with open("line.json", "w") as file:
+    # Write the JSON data to a test file
+    test_file = "line.json"
+    with open(test_file, "w") as file:
         file.write(json_data)
-    # Read the JSON data from the file
-    with open("line.json", "r") as file:
+    # Read the JSON data from the test file
+    with open(test_file, "r") as file:
         json_data = json.loads(file.read())
     # Parse the JSON data back into a Line object
     loaded_line = Line(**json_data)
+    # Remove the test file
+    os.remove(test_file)
+    # Validate loaded Line object
     assert line == loaded_line
