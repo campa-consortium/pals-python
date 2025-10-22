3dda253d0ff2caeb32d36d46c119d84a50d6e293
Axel Huebl <axel.huebl@plasma.ninja>
2025-10-22 14:16:28 -0700

    Clean Up Example

--- examples/fodo.py
+++ examples/fodo.py
@@ -1,10 +1,5 @@
 import json
-import os
-import sys
 import yaml
-
-# Add the parent directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/")))
 
 from pals import MagneticMultipoleParameters
 from pals import Drift
