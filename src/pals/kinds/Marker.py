b95cc08d02285eae482454b7e91ce60d2974d372
Axel Huebl <axel.huebl@plasma.ninja>
2025-10-29 11:22:20 -0700

    `Marker`: Finalize
    Remove the construction notice from `Marker`.
    
    - improve docstring
    - introduce `ThinElement` helper class

--- src/pals/kinds/Marker.py
+++ src/pals/kinds/Marker.py
@@ -1,12 +1,13 @@
 from typing import Literal
 
-from .mixin import BaseElement
-from .utils import under_construction
+from .mixin import ThinElement
 
 
-@under_construction("Marker")
-class Marker(BaseElement):
-    """Zero length element to mark a particular position"""
+class Marker(ThinElement):
+    """Zero length element to mark a particular position
+
+    The main purpose of this thin element is to name a position in the beamline.
+    """
 
     # Discriminator field
     kind: Literal["Marker"] = "Marker"
--- src/pals/kinds/mixin/__init__.py
+++ src/pals/kinds/mixin/__init__.py
@@ -1,2 +1,3 @@
 from .BaseElement import BaseElement  # noqa: F401
 from .ThickElement import ThickElement  # noqa: F401
+from .ThinElement import ThinElement  # noqa: F401
